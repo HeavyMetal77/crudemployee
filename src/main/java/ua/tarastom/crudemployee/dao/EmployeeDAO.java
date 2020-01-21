26cdcb235591f560d172f253d80e321bb27e82db
HeavyMetal77 <ttm2006@ukr.net>
2020-01-21 20:09:19 +0200

    add methods: delete, saveOrUpdate, find(by Id)

--- src/main/java/ua/tarastom/crudemployee/dao/EmployeeDAO.java
+++ src/main/java/ua/tarastom/crudemployee/dao/EmployeeDAO.java
@@ -6,4 +6,11 @@ import java.util.List;
 
 public interface EmployeeDAO {
     List<Employee> findAll();
+
+    Employee findEmployee(int id);
+
+    Employee saveOrUpdate(Employee employee);
+
+    void deleteEmployee(int id);
+
 }
--- src/main/java/ua/tarastom/crudemployee/dao/EmployeeDAOHibermateImpl.java
+++ src/main/java/ua/tarastom/crudemployee/dao/EmployeeDAOHibermateImpl.java
@@ -17,16 +17,34 @@ public class EmployeeDAOHibermateImpl implements EmployeeDAO{
     private EntityManager entityManager;
 
     @Override
-    @Transactional
     public List<Employee> findAll() {
-
         Session currentSession = entityManager.unwrap(Session.class);
-
         //Var.1 using Hibernate API native method
         Query<Employee> theQuery = currentSession.createQuery("from Employee", Employee.class);
         List<Employee> employees = theQuery.getResultList();
-
         return employees;
+    }
+
+    @Override
+    public Employee findEmployee(int id) {
+        Session currentSession = entityManager.unwrap(Session.class);
+        Employee employee = currentSession.get(Employee.class, id);
+        return employee;
+    }
+
+    @Override
+    public Employee saveOrUpdate(Employee employee) {
+        Session currentSession = entityManager.unwrap(Session.class);
+        currentSession.saveOrUpdate(employee);
+        return employee;
+    }
+
+    @Override
+    public void deleteEmployee(int id) {
+        Session currentSession = entityManager.unwrap(Session.class);
+        Query query = currentSession.createQuery("delete from Employee where id=:employeeId");
+        query.setParameter("employeeId", id);
+        query.executeUpdate();
     }
 }
 
